409a3c31a9c846eb51407e013b5c6ed1045abc62
morgan <morgan.keys@gmail.com>
2019-09-18 19:45:47 -0700

    Create default frame

--- react-sandbox/src/App.js
+++ react-sandbox/src/App.js
@@ -1,25 +1,29 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+
+import styled from 'styled-components'
+
+const Frame = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+
+  background-color: #444;
+  color: #fff;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Frame>
+      Hello world!
+    </Frame>
   );
 }
 
