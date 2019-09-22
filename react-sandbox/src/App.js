972481735498af14f1b304db41b5a8d03402b116
morgan <morgan.keys@gmail.com>
2019-09-21 20:50:58 -0700

    Figured out column-wrapping—align content is what I needed...

--- react-sandbox/src/App.js
+++ react-sandbox/src/App.js
@@ -18,11 +18,44 @@ const Frame = styled.div`
   align-items: center;
 `
 
+const Box = styled.div`
+  height: 200px;
+  width: 200px;
+  background-color: ${props => props.color}
+`
+const TopLine = styled.div`
+  font-weight: bold;
+  line-height: 22px;
+  font-size: 16px;
+`
+const BottomLine = styled.div`
+  font-weight: normal;
+  line-height: 19px;
+  font-size: 14px;
+`
+
+const Cell = styled.div`
+  border: 1px solid #fff;
+  padding: 0 8px;
+  width: ${props => props.width}
+  height: ${props => props.height}
+  display: flex;
+  justify-content: center;
+  align-content: start;
+  flex-flow: column wrap;
+
+  * {
+    margin-right: 12px;
+  }
+`
 
 function App() {
   return (
     <Frame>
-      Hello world!
+      <Cell width="500px" height="20px">
+        <TopLine>Morgan Keys</TopLine>
+        <BottomLine>Desigineer Extrodinaire</BottomLine>
+      </Cell>
     </Frame>
   );
 }
