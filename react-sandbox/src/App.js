2028993fb155813ef8649095e7a92975e13d5908
morgan <morgan.keys@gmail.com>
2019-09-26 19:54:53 -0700

    Aligning to baseline is rad

--- react-sandbox/src/App.js
+++ react-sandbox/src/App.js
@@ -3,6 +3,33 @@ import logo from './logo.svg';
 
 import styled from 'styled-components'
 
+const colors = {
+  "NAVY": "#001f3f",
+  "BLUE": "#0074D9",
+  "AQUA": "#7FDBFF",
+  "TEAL": "#39CCCC",
+  "OLIVE": "#3D9970",
+  "GREEN": "#2ECC40",
+  "LIME": "#01FF70",
+  "YELLOW": "#FFDC00",
+  "ORANGE": "#FF851B",
+  "RED": "#FF4136",
+  "MAROON": "#85144b",
+  "FUCHSIA": "#F012BE",
+  "PURPLE": "#B10DC9",
+  "BLACK": "#111111",
+  "GRAY": "#AAAAAA",
+  "SILVER": "#DDDDDD",
+}
+
+function getRandomColorIndex() {
+  return Math.floor(Math.random() * (15)); //The maximum is inclusive and the minimum is inclusive 
+}
+
+function getRandomHeight() {
+  return Math.floor(Math.random() * 200 + 20); //The maximum is inclusive and the minimum is inclusive 
+}
+
 const Frame = styled.div`
    position: absolute;
    top: 0;
@@ -10,37 +37,53 @@ const Frame = styled.div`
    left: 0;
    right: 0;
 
-   background-color: #444;
-   color: #fff;
+   background-color: #fff;
+   color: #191919;
 
    display: flex;
-   justify-content: center;
-   align-items: center;
+   flex-flow: row wrap;
+   justify-content: start;
+   align-items: baseline;
+   align-content: start;
+   overflow: scroll;
+   padding: 48px;
+`
+
+const Box = styled.div`
+  height: ${props => props.height}px;
+  width: 200px;
+  background-color: ${props => Object.values(colors)[props.colorIndex]};
+  margin: 30px;
+  display: flex;
+
+  justify-content: center;
+  align-content: center;
+  align-items: center;
 `
 
 class App extends React.Component {
   constructor(props) {
-     super(props)
-     this.state = {
-        height: 40
-     }
-     
-     this.changeHeight = this.changeHeight.bind(this)
+    super(props)
+    this.state = {}
   }
-  
-  changeHeight(event) {
-     this.setState({height: event.target.value})
-  }
-  
+
+
   render() {
-     return (
-     <Frame>
-           <h3>
-              Hello world!
-           </h3>
-     </Frame>
-  );
-}
+    return (
+      <Frame>
+        <Box colorIndex={getRandomColorIndex()} height={getRandomHeight()}><p>Cat</p></Box>
+        <Box colorIndex={getRandomColorIndex()} height={getRandomHeight()}><p>Cat</p></Box>
+        <Box colorIndex={getRandomColorIndex()} height={getRandomHeight()}><p>Cat</p></Box>
+        <Box colorIndex={getRandomColorIndex()} height={getRandomHeight()}><p>Cat</p></Box>
+        <Box colorIndex={getRandomColorIndex()} height={getRandomHeight()}><p>Cat</p></Box>
+        <Box colorIndex={getRandomColorIndex()} height={getRandomHeight()}><p>Cat</p></Box>
+        <Box colorIndex={getRandomColorIndex()} height={getRandomHeight()}><p>Cat</p></Box>
+        <Box colorIndex={getRandomColorIndex()} height={getRandomHeight()}><p>Cat</p></Box>
+        <Box colorIndex={getRandomColorIndex()} height={getRandomHeight()}><p>Cat</p></Box>
+        <Box colorIndex={getRandomColorIndex()} height={getRandomHeight()}><p>Cat</p></Box>
+      </Frame>
+    );
+  }
 }
 
 export default App;
