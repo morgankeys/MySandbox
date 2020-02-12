8b1857a5755a02b73399ab4b4faf242edb9181dd
morgan <morgan.keys@gmail.com>
2020-02-11 23:30:17 -0800

    Add simple input and  data

--- react-sandbox/src/App.js
+++ react-sandbox/src/App.js
@@ -14,33 +14,79 @@ const Frame = styled.div`
    color: #fff;
 
    display: flex;
+   flex-direction: column;
    justify-content: center;
    align-items: center;
 `
 
+const Input = styled.input`
+   height: 40px;
+   width: 400px;
+   font-size: 32px;
+   margin-bottom: 24px;
+`
+
+const Suggester = styled.p`
+   display: flex;
+   flex-direction: column;
+   border-radius: 8px;
+
+   button {
+      height: 32px;
+      font-size: 18px;
+      border-radius: 4px;
+      background-color: #2D9CDB;
+      color: #fff;
+      border: none;
+
+      &:active {
+         background-color: #2D9CDB44;
+      }
+   }
+`
+
 class App extends React.Component {
-  constructor(props) {
-     super(props)
-     this.state = {
-        height: 40
-     }
-     
-     this.changeHeight = this.changeHeight.bind(this)
-  }
-  
-  changeHeight(event) {
-     this.setState({height: event.target.value})
-  }
-  
-  render() {
-     return (
-     <Frame>
-           <h3>
-              Hello world!
+   constructor(props) {
+      super(props)
+      this.state = {
+         entries: ["hello"],
+      }
+
+      this.getSuggestion = this.changeHeight.bind(this)
+      this.addToList = this.changeHeight.bind(this)
+      this.removeFromList = this.changeHeight.bind(this)
+   }
+
+   changeHeight(event) {
+      this.setState({ height: event.target.value })
+   }
+
+   getSuggestion(event) {
+      
+   }
+
+   addToList(event) {
+      this.setState({ entries: this.state.entries.push(event.target.value) })
+   }
+
+   removeFrom
+
+   render() {
+      return (
+         <Frame>
+            <h3>
+               Hello world!
            </h3>
-     </Frame>
-  );
-}
+            <Suggester>
+               <Input type="text" placholder="Type keywords here" />
+               <button onClick={this.addToList}>Add to list</button>
+            </Suggester>
+            <p>
+               {this.state.entries.map(word => <p>{word}</p>)}
+            </p>
+         </Frame>
+      );
+   }
 }
 
 export default App;
---
+++ react-sandbox/src/csi-codes.js
@@ -0,0 +1,141 @@
+export const csiCodes = [
+    {
+        code: '00000',
+        description: 'Miscellaneous',
+    },
+    {
+        code: '00010',
+        description: 'Accountant',
+    },
+    {
+        code: '00020',
+        description: 'Advertising/Public Relations',
+    },
+    {
+        code: '00030',
+        description: 'Attorney',
+    },
+    {
+        code: '00040',
+        description: 'Architect',
+    },
+    {
+        code: '00050',
+        description: 'Auctioneer',
+    },
+    {
+        code: '00060',
+        description: 'Automotive, Trucks & Trailers – Sales, Repair, Parts',
+    },
+    {
+        code: '00070',
+        description: 'Blue Printing',
+    },
+    {
+        code: '00075',
+        description: 'Construction Materials – General',
+    },
+    {
+        code: '00080',
+        description: 'Business Service',
+    },
+    {
+        code: '00090',
+        description: 'Caterer',
+    },
+    {
+        code: '00100',
+        description: 'Communications Equip. & Service',
+    },
+    {
+        code: '00110',
+        description: 'Computers, Hardware, Software',
+    },
+    {
+        code: '00120',
+        description: 'Construction Equip. Sales, Leasing, Repair',
+    },
+    {
+        code: '00130',
+        description: 'Construction Tools',
+    },
+    {
+        code: '00140',
+        description: 'Consultant',
+    },
+    {
+        code: '00150',
+        description: 'Developer',
+    },
+    {
+        code: '00160',
+        description: 'Educational Schools, Seminars, Training',
+    },
+    {
+        code: '00170',
+        description: 'Employee Benefits',
+    },
+    {
+        code: '00180',
+        description: 'Engineer',
+    },
+    {
+        code: '00190',
+        description: 'Financial Service',
+    },
+    {
+        code: '00195',
+        description: 'Government',
+    },
+    {
+        code: '00200',
+        description: 'Insurance – Surety',
+    },
+    {
+        code: '00220',
+        description: 'Office Equip. & Supplies',
+    },
+    {
+        code: '00230',
+        description: 'Office Trailers',
+    },
+    {
+        code: '00240',
+        description: 'Personnel Recruitment',
+    },
+    {
+        code: '00245',
+        description: 'Petroleum Products',
+    },
+    {
+        code: '00260',
+        description: 'Publications/Printing',
+    },
+    {
+        code: '00270',
+        description: 'Public Utility',
+    },
+    {
+        code: '00290',
+        description: 'Safety/First Aid Supplies',
+    },
+    {
+        code: '00300',
+        description: 'Sanitation – Collections',
+    },
+    {
+        code: '00310',
+        description: 'Securities & Investments',
+    },
+    {
+        code: '00320',
+        description: 'Transportation/Freight',
+    },
+    {
+        code: '00330',
+        description: 'Wire Ropes/Cables',
+    },
+    {
+        code: '00340',
+        description: 'Waste – Hazardous, Toxic ',
+    },]
