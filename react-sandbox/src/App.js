74209ea5e9aea5fad4d9f1aad662e762ead30c37
Morgan Keys <morgan.keys@gmail.com>
2020-09-09 19:18:15 -0500

    Successfully pass object into drag handler, now need to be able to set the state for just that object

--- react-sandbox/src/App.js
+++ react-sandbox/src/App.js
@@ -1,147 +1,151 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from "react";
+import logo from "./logo.svg";
 
-import styled from 'styled-components'
+import styled from "styled-components";
 
-import Draggable from 'react-draggable';
-
+import Draggable from "react-draggable";
 
 const Frame = styled.div`
-   position: absolute;
-   top: 0;
-   bottom: 0;
-   left: 0;
-   right: 0;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
 
-   background-color: #444;
-   color: #fff;
+  background-color: #444;
+  color: #fff;
 
-   display: flex;
-   justify-content: center;
-   align-items: center;
-   flex-direction: column;
-`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
 
 const StyledBucket = styled.div`
-   width: 300px;
-   height: 300px;
-   border: 1px solid #fff;
+  width: 300px;
+  height: 300px;
+  border: 1px solid #fff;
 
-   display: flex;
-   justify-content: center;
-   align-items: center;
-`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 const Bucket = (props) => {
-   return (
-      <StyledBucket>
-         {props.children}
-      </StyledBucket>
-   )
-}
+  return <StyledBucket>{props.children}</StyledBucket>;
+};
 
 const StyledLetterBox = styled.div`
-   border: 1px solid #fff;
-   width: 60px;
-   height: 60px;
-   margin: 16px;
+  border: 1px solid #fff;
+  width: 60px;
+  height: 60px;
+  margin: 16px;
 
-   display: flex;
-   justify-content: center;
-   align-items: center;
-`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 const LetterBox = (props) => {
-   return (
-      <Draggable onDrag={props.onDrag}>
-         <StyledLetterBox>{props.children}</StyledLetterBox>
-      </Draggable>
-   )
-}
+  return (
+    <Draggable onDrag={props.onDrag}>
+      <StyledLetterBox>{props.children}</StyledLetterBox>
+    </Draggable>
+  );
+};
 
 const Half = styled.div`
-   width: 100%;
-   height: 50%;
+  width: 100%;
+  height: 50%;
 
-   display: flex;
-   justify-content: space-around;
-   align-items: center;
-`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+`;
 
 const Logger = styled.div`
-   position: absolute;
-   bottom: 0;
-   left: 0;
-`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+`;
 
 class App extends React.Component {
   constructor(props) {
-     super(props)
-     this.state = {
-         height: 40,
-         letters: [
-            "A", "B", "C", "D",
-            "E", "F", "G", "H",
-            "I", "J", "K", "L",
-            "M", "N", "O", "P"
-         ],
-         letterObjects: {},
-         bucketLeft: {
-            x: null,
-            y: null,
-            contains: [],
-         },
-         bucketRight: {
-            x: null,
-            y: null,
-            contains: [],
-         },
-         logger: `Logging: null`
-     }
-     
-     this.dragHandler = this.dragHandler.bind(this)
-  }
-  
-  dragHandler(event, data, letterObject) {
-      this.setState({
-      //   letterObjects[letterObject.letter]:
-        logger: `Logging: x:${data.x} y:${data.y}`})
+    super(props);
+    const letters = [
+      "A",
+      "B",
+      "C",
+      "D",
+      "E",
+      "F",
+      "G",
+      "H",
+      "I",
+      "J",
+      "K",
+      "L",
+      "M",
+      "N",
+      "O",
+      "P",
+    ];
+    let letterObjects = [];
+    letters.map( l =>
+      letterObjects.push({
+        letter: l,
+        x: null,
+        y: null,
+        sorted: false,
+      })
+    );
+
+    console.log(letterObjects)
+
+    this.state = {
+      height: 40,
+      letterObjects: letterObjects,
+      bucketLeft: {
+        x: null,
+        y: null,
+        contains: [],
+      },
+      bucketRight: {
+        x: null,
+        y: null,
+        contains: [],
+      },
+      logger: `Logging: null`,
+    };
+
+    this.dragHandler = this.dragHandler.bind(this);
   }
 
-  createLetterObjects(letters) {
-      this.state.letters.map(l => 
-         this.state.letterObjects[l] = {
-            letter: l,
-            x: null,
-            y: null,
-            sorted: false
-         }
-      )
-  }
-  
+  dragHandler = (letter) => (event, data) => {
+    this.setState({
+      logger: `Logging: x:${data.x} y:${data.y}`,
+    });
+  };
+
   render() {
-     this.createLetterObjects()
-     console.log(this.state.letterObjects)
-
-     return (
-     <Frame>
-           <Half key="top">
-               {this.state.letterObjects.map(l => 
-                  <LetterBox 
-                     key={l} 
-                     onDrag={
-                        this.dragHandler(l)
-                     }>{l}</LetterBox>
-               )}
-            </Half>
-            <Half key="bottom">
-               <Bucket key="left">Left Bucket</Bucket>
-               <Bucket key="right">Right Bucket</Bucket>
-            </Half>
-            <Logger>{this.state.logger}</Logger>
-     </Frame>
-  );
-}
+    console.log(this.state)
+    return (
+      <Frame>
+        <Half key="top">
+          {this.state.letterObjects.map( l => (
+            <LetterBox key={l.letter} onDrag={this.dragHandler(l)}>
+              {l.letter}
+            </LetterBox>
+          ))}
+        </Half>
+        <Half key="bottom">
+          <Bucket key="left">Left Bucket</Bucket>
+          <Bucket key="right">Right Bucket</Bucket>
+        </Half>
+        <Logger>{this.state.logger}</Logger>
+      </Frame>
+    );
+  }
 }
 
 export default App;
