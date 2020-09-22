dfe332e0cfc991faeb35f1f89905ff37f3a750d4
Morgan Keys <morgan.keys@gmail.com>
2020-09-21 18:38:31 -0700

    Made a basic typing buffer

--- vanilla-js/scripts/main.js
+++ vanilla-js/scripts/main.js
@@ -1,2 +1,23 @@
 const appDiv = document.querySelector('.app');
-appDiv.innerHTML = '<div class="frame"><h1>Hello World!!</h1></div>';
+
+let buffer = []
+
+const handleKeypress = (event) => {
+    if (event.code == "Backspace" || event.code == "Delete") {
+        buffer.pop()
+    } else {
+        buffer.push(event.key)
+    }
+    update()
+}
+
+const update = () => {
+    let bufferString = ''
+    for (let i = 0; i<buffer.length; i++) {
+        bufferString = bufferString+buffer[i]
+    }
+    appDiv.innerHTML = `<div class="frame"><h1>${bufferString}<span class="cursor">|</h1></div>`
+}
+
+appDiv.innerHTML = `<div class="frame"><h1><span class="cursor">|</h1></div>`
+document.addEventListener('keydown', handleKeypress);
