8e170605325796630089b05fc1e419953b6901e8
Morgan Keys <morgan.keys@gmail.com>
2024-08-14 09:25:12 -0600

    Create webcomponents branch, try hello world example

--- vanilla-js/scripts/main.js
+++ vanilla-js/scripts/main.js
@@ -1,2 +1,21 @@
 const appDiv = document.querySelector('.app');
 appDiv.innerHTML = '<div class="frame"><h1>Hello World!!</h1></div>';
+
+class MyElement extends HTMLElement {
+    constructor() {
+      super();
+      // Attach a shadow DOM to the custom element.
+      const shadow = this.attachShadow({ mode: 'open' });
+  
+      // Create some content inside the shadow DOM.
+      const wrapper = document.createElement('span');
+      wrapper.textContent = "Hello, I'm a custom element!";
+  
+      // Append the created elements to the shadow DOM.
+      shadow.appendChild(wrapper);
+    }
+  }
+  
+  // Define the new custom element.
+  customElements.define('my-element', MyElement);
+  
